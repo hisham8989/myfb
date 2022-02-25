788565d1d8c977d7f74d4280980eb437e1483ca8
hisham8989 <khanhisham25@gmail.com>
2022-02-26 02:38:10 +0530

    setup layout using express ejs layouts

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const path = require('path');
-const port = 9000;
-const app = express();
+const express = require('express')
+const path = require('path')
+const port = 9000
+const app = express()
+const expresLayouts = require('express-ejs-layouts')
 
+app.use(expresLayouts)
+
+// Extract Style & Script from sub pages
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
 
 //View Templetes for for Front End
-app.set('view engine','ejs');
-app.set('views','./views')
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 // Serving Static Files
 app.use(express.static('assets'))
 
-
 // Use Express Router Middleware
-app.use('/',require('./routes'))
+app.use('/', require('./routes'))
 
-
-app.listen(port,function(err){
-    if(err){
-        console.log("Error in running server".err);
-        return
-    }
-    console.log(`Server is running on ${port} .....`);
+app.listen(port, function (err) {
+  if (err) {
+    console.log('Error in running server'.err)
+    return
+  }
+  console.log(`Server is running on ${port} .....`)
 })
