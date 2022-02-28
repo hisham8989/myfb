0463158cab73d92822a9fb095337ab41fe1ce7fd
hisham8989 <khanhisham25@gmail.com>
2022-03-01 04:15:35 +0530

    add posts to frontend

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
-module.exports.home = function(req,res){
-    return res.render('home',{
-        title:"Codiel | Home"
+const Post = require('../models/post')
+
+module.exports.home = function (req, res) {
+  //   Post.find({}, function (err, posts) {
+  //     return res.render('home', {
+  //       title: 'Codiel | Home',
+  //       posts,
+  //     })
+  //   })
+
+  Post.find({})
+    .populate('user')
+    .exec(function (err, posts) {
+      return res.render('home', {
+        title: 'Codiel | Home',
+        posts,
+      })
     })
 }
