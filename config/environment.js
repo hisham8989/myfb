eabb2acb47daef0dbad7a8d6762e0a0469685f06
hisham8989 <khanhisham25@gmail.com>
2022-03-20 02:24:52 +0530

    update exports

--- config/environment.js
+++ config/environment.js
@@ -64,4 +64,4 @@ const production = {
   }
 }
 
-module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV)
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT)
